4b74720b89e2cd320bea633f455373262f0ed7b9
Jacobo <correo>
2025-03-20 22:08:14 +0100

    1º commit ej1

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Ej-1-JSON
 Ej 1 del tema 7 API's
+
+Información para crear el .json:
+Audi España
+https://www.audi.es/es/?csref=sea:PHD:bc:00004383_NC_AO_CPC_BRA_BRA_BRD_ESP_NU_SEAH_PSEAC_NU_TEAD_NU_GAD_GADW_CPC_NU_NU_NU_NU_CXD_ES_PURB_EX_AudiESNewCars-BT-EXT-2205023-BRAND-PURE-PHD2025011000160:s:Google:google.es:p~exact:fn~marca_pura:c~audi:mt~p&gad_source=1&gclid=Cj0KCQjw-e6-BhDmARIsAOxxlxWOGG0UAX2P_kMUa_SsCOoJ6Jg1DH4XNGXcpoCYE5PJHdHeug5UnmwaAgGHEALw_wcB&gclsrc=aw.ds
---
+++ main.py
@@ -0,0 +1,20 @@
+import json
+
+datos = open(json.load("datos.json"))
+
+# Quien es el fundador de Audi y cuando lo fundo. Ejemplo: Flautin el flautas creó Audi en el año 1679
+print(f"{datos["fundacion"]["fundador"]} creó Audi en el año {datos["fundacion"]["año"]}")
+
+# Cual es el eslogan de Audi. Ejemplo: Eslogan: Las mariposas vuelan
+
+
+# Mostrar el nombre de cada tecnología seguido de su descripción, con un salto de línea entre cada una"
+
+
+# Modelos populares de Audi en forma de lista
+
+
+# Las principales competencias de Audi dadas la vuelta
+
+
+# Mostrar el modelo popular número 3 seguido de la descripción del elemento 5 de tecnologia_destacada
